56d8f5eba7bccd75899acadfce73c5b6b5e8526b
ataylor <github@rxtaylor.com>
2013-12-23 17:11:05 -0500

    Initial commit:  Testing CSS3 and sizes on mobile devices.

--- MobileKinetics/index.php
+++ MobileKinetics/index.php
@@ -3,37 +3,27 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
-        <style>
-           .blah{-webkit-animation: fadein 0.5s ease-in forwards;}
-           @-webkit-keyframes fadein{
-            from{opacity:0;}
-            to{opacity:1;}
-            }
-        </style>
+        <link rel="stylesheet" href="rsc/main.css" type="text/css"/>
+        <link rel="shortcut icon" href="rsc/favicon.ico" />
     </head>
     <body>
-        <p id="testerific">Answer here.</p>
-        <input type="text" id="weight" /><br>
-        <input type="text" id="height" /><br>
-        <button onclick="Test();">Click ME!</button>
-        <hr width="480px">
-        <div id="bob" style="opacity:0;">
-        <h1>Ignore the crap above...</h1>
-        Text:<input type="text"><br>
-        Email:<input type="email"><br>
-        Tel:<input type="tel"><br>
-        Number:<input type="number"><br>
-        Password:<input type="password"><br>
-        Date:<input type="date"><br>
-        DateTime:<input type="datetime"><br>
-        Month:<input type="month"><br>
-        Search:<input type="search"><br>
+        <div id="question1" class="blah">
+            <button onclick="Test('new');">New Dosing</button>
+            <br>
+            <button onclick="Test('adjustment');">Adjustment</button>
         </div>
+        <div id="newdose" class="gone">New Dose!!!</div>
+        <div id="adjustment" class="gone">Adjustment!!!</div>
     </body>
     <script type="text/javascript" src="js/pharmcalc.js"></script>
     <script>
-    function Test(){
-        document.getElementById("bob").className="blah";
+    function Test(one){
+        if(one==="new"){
+            document.getElementById("newdose").className="blah";
+        }else{
+            document.getElementById("adjustment").className="blah";
+        }
+        document.getElementById("question1").className="gone";
     };
     </script>
 </html>
